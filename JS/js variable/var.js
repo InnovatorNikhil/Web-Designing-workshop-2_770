4822719bce03fef9f97b774312c95a87b7f70db2
Innovator Nikhil <nikkdubey23@gmail.com>
2026-04-13 01:33:13 +0530

    variable concept

---
+++ JS/js variable/var.js
@@ -0,0 +1,23 @@
+var a=10;
+console.log(a);
+
+
+
+
+
+let marks=95;
+console.log(marks);
+
+if (true){
+    let marks1=85;
+}
+console.log(marks1);
+
+
+let name="john";
+console.log(name);
+
+
+
+const pi=3.14;
+console.log(pi);
