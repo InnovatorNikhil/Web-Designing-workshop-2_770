34edbb1ad24b854c0ad5d209284ef224ed045eb8
Innovator Nikhil <nikkdubey23@gmail.com>
2026-06-20 03:25:42 +0530

    datatype completed succesfully

---
+++ javascriptpractice/01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all code as newer version
+
+alert(3+3)   //node js not browser
+
+console.log(3+3)  //code readibility should be high
+console.log("hello")
+
+
+
+
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+let state;
+
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true or false
+//null=>standalone value
+//undefined=>
+//symbol=>uniqueness
+
+
+//object
+console.log(typeof null);//object
+console.log(typeof undefined)  //undefined
