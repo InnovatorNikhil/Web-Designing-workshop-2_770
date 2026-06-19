2aecac633400db9362ef2511f8a8755c35fc1de4
Innovator Nikhil <nikkdubey23@gmail.com>
2026-06-20 03:08:24 +0530

    variable completed succesfully

---
+++ javascriptpractice/01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountEmail="nikhild04@gmail.com"
+var accountPassword="12345"
+accountCity="varanasi"
+let accountState;
+
+//accountId=2// mot allowed
+
+
+accountEmail="hc@hc.com"
+accountPassword="2121121"
+accountCity="bengluru"
+console.log(accountId);
+
+// prefer not to use var bacause of issue in block scope and functional scope
+
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
